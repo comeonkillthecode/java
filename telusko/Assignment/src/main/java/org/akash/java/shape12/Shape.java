93e4b946ebf187008807241d059b1ee2757bdca5
akashboora <booraakash@gmail.com>
2024-08-04 22:28:25 +0530

    telusko: assignment-12

---
+++ telusko/Assignment/src/main/java/org/akash/java/shape12/Shape.java
@@ -0,0 +1,6 @@
+package org.akash.java.shape12;
+
+public interface Shape {
+    double area();
+    double perimeter();
+}
---
+++ telusko/Assignment/src/main/java/org/akash/java/shape12/Triangle.java
@@ -0,0 +1,67 @@
+package org.akash.java.shape12;
+
+public class Triangle implements Shape {
+    private double base;
+    private double height;
+    private double sideA;
+    private double sideB;
+    private double sideC;
+
+    public Triangle(double base, double height, double sideA, double sideB, double sideC) {
+        this.base = base;
+        this.height = height;
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    @Override
+    public double area() {
+        return 0.5 * base * height;
+    }
+
+    @Override
+    public double perimeter() {
+        return sideA + sideB + sideC;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getSideA() {
+        return sideA;
+    }
+
+    public void setSideA(double sideA) {
+        this.sideA = sideA;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    public void setSideB(double sideB) {
+        this.sideB = sideB;
+    }
+
+    public double getSideC() {
+        return sideC;
+    }
+
+    public void setSideC(double sideC) {
+        this.sideC = sideC;
+    }
+}
