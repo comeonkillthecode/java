0bf7a227611c08c9d7b8b5489e4b77fc5944d95e
akashboora <booraakash@gmail.com>
2024-08-04 22:28:25 +0530

    telusko: assignment-12

---
+++ telusko/Assignment/src/main/java/org/akash/java/shape12/Square.java
@@ -0,0 +1,27 @@
+package org.akash.java.shape12;
+
+public class Square implements Shape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double area() {
+        return side * side;
+    }
+
+    @Override
+    public double perimeter() {
+        return 4 * side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+}
