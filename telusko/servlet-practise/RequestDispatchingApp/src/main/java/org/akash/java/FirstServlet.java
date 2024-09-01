b2d7d7780e1146150882f952118e94b635e6b5d8
akashboora <booraakash@gmail.com>
2024-09-01 18:51:24 +0530

    telusko: request disptaching

---
+++ telusko/servlet-practise/RequestDispatchingApp/src/main/java/org/akash/java/FirstServlet.java
@@ -0,0 +1,29 @@
+package org.akash.java;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Control in first service");
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/SecondServlet");
+
+//        requestDispatcher.forward(req, resp);
+        requestDispatcher.include(req, resp);
+
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("<h1>Response from Servlet one<h1>");
+        printWriter.close();
+
+        System.out.println("Control in again first service");
+
+    }
+}
