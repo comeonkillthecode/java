6dd03c385ef90384f160ff9ffb28bf6d83b06544
akashboora <booraakash@gmail.com>
2024-09-02 21:47:22 +0530

    telusko: added HTTPSession

--- telusko/servlet-practise/RequestDispatchingApp/src/main/java/org/akash/java/SecondServlet.java
+++ telusko/servlet-practise/RequestDispatchingApp/src/main/java/org/akash/java/SecondServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -14,8 +15,13 @@ public class SecondServlet extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Control in second service");
 
+        HttpSession session = req.getSession(false);
+
+        String name = (String) session.getAttribute("name");
+        String city = (String) session.getAttribute("city");
+
         PrintWriter printWriter = resp.getWriter();
-        printWriter.println("<h1>Response from Servlet two<h1>");
+        printWriter.println("<h1>Response from Servlet two<h1>"+"<h2>"+name+ city +"</h2>");
         printWriter.close();
     }
 }
