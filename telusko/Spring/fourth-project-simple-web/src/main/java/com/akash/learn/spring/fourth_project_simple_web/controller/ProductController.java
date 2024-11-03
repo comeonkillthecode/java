18e4a362df26ec22a0d075e31b5afe89a39ea89c
akashboora <booraakash@gmail.com>
2024-11-03 19:52:41 +0530

    learn/spring: post get by id

--- telusko/Spring/fourth-project-simple-web/src/main/java/com/akash/learn/spring/fourth_project_simple_web/controller/ProductController.java
+++ telusko/Spring/fourth-project-simple-web/src/main/java/com/akash/learn/spring/fourth_project_simple_web/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.akash.learn.spring.fourth_project_simple_web.controller;
 import com.akash.learn.spring.fourth_project_simple_web.model.Product;
 import com.akash.learn.spring.fourth_project_simple_web.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,8 +12,18 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProducts(){
         return productService.getProducts();
+    }
+
+    @GetMapping("/products/{productId}")
+    public Product getProductById(@PathVariable int productId){
+        return productService.getProductById(productId);
+    }
+
+    @PostMapping("/products")
+    public Product createProduct(@RequestBody Product product){
+        return productService.createProduct(product);
     }
 }
--- telusko/Spring/fourth-project-simple-web/src/main/java/com/akash/learn/spring/fourth_project_simple_web/service/ProductService.java
+++ telusko/Spring/fourth-project-simple-web/src/main/java/com/akash/learn/spring/fourth_project_simple_web/service/ProductService.java
@@ -3,17 +3,27 @@ package com.akash.learn.spring.fourth_project_simple_web.service;
 import com.akash.learn.spring.fourth_project_simple_web.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
-    private List<Product> products = Arrays.asList(
+    private List<Product> products = new ArrayList<>(Arrays.asList(
             new Product(101, "IPhone", 50000),
             new Product(102, "Google", 25000),
-            new Product(103, "Oneplus", 20000));
+            new Product(103, "Oneplus", 20000)));
 
     public List<Product> getProducts(){
         return products;
+    }
+
+    public Product getProductById(int productId){
+        return products.stream().filter(p -> p.getId() == productId).findFirst().get();
+    }
+
+    public Product createProduct(Product product) {
+        products.add(product);
+        return product;
     }
 }
