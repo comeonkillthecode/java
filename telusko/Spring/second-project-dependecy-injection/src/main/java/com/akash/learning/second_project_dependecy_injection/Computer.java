a6139e273d39da1d775ac177c09f90a38407c165
akashboora <booraakash@gmail.com>
2024-11-03 11:20:29 +0530

    learn/spring: Autowiring(field, constructor, setter) primary and qualifier

---
+++ telusko/Spring/second-project-dependecy-injection/src/main/java/com/akash/learning/second_project_dependecy_injection/Computer.java
@@ -0,0 +1,5 @@
+package com.akash.learning.second_project_dependecy_injection;
+
+public interface Computer {
+    public void compile();
+}
--- telusko/Spring/second-project-dependecy-injection/src/main/java/com/akash/learning/second_project_dependecy_injection/Dev.java
+++ telusko/Spring/second-project-dependecy-injection/src/main/java/com/akash/learning/second_project_dependecy_injection/Dev.java
@@ -1,11 +1,30 @@
 package com.akash.learning.second_project_dependecy_injection;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Component
 public class Dev {
+
+    // Field Injection
+    @Autowired
+    @Qualifier("laptop")
+    private Computer comp;
+
+//    constructor injection
+//    public Dev(Laptop laptop) {
+//        this.laptop = laptop;
+//    }
+
+//    setter injection
+//    @Autowired
+//    public void setLaptop(Laptop laptop) {
+//        this.laptop = laptop;
+//    }
+
     public void learn(){
+        comp.compile();
         System.out.println("Learning Spring Boot");
     }
 }
