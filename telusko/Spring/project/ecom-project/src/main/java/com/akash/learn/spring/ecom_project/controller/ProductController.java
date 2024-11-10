11473ac10b65faab2da08a809e13cfced76d4773
akashboora <booraakash@gmail.com>
2024-11-10 18:05:10 +0530

    learn/spring/project: added create and update for products

--- telusko/Spring/project/ecom-project/src/main/java/com/akash/learn/spring/ecom_project/controller/ProductController.java
+++ telusko/Spring/project/ecom-project/src/main/java/com/akash/learn/spring/ecom_project/controller/ProductController.java
@@ -4,8 +4,10 @@ import com.akash.learn.spring.ecom_project.model.Product;
 import com.akash.learn.spring.ecom_project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -32,4 +34,21 @@ public class ProductController {
         }
     }
 
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile){
+        try {
+            Product product1 = service.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId){
+        Product product = service.getProductById(productId);
+        byte[] imageFile = product.getImageData();
+
+        return ResponseEntity.ok().contentType(MediaType.valueOf(product.getImageType())).body(imageFile);
+    }
 }
--- telusko/Spring/project/ecom-project/src/main/java/com/akash/learn/spring/ecom_project/service/ProductService.java
+++ telusko/Spring/project/ecom-project/src/main/java/com/akash/learn/spring/ecom_project/service/ProductService.java
@@ -4,7 +4,9 @@ import com.akash.learn.spring.ecom_project.model.Product;
 import com.akash.learn.spring.ecom_project.repo.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -20,5 +22,12 @@ public class ProductService {
 
     public Product getProductById(int productId) {
         return productRepository.findById(productId).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return productRepository.save(product);
     }
 }
